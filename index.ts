b0236e37d276496281dcf25d4cb7124770733b82
sakulstra <lukasstrassel@googlemail.com>
2024-04-17 15:36:53 +0200

    feat: improve cache

--- index.ts
+++ index.ts
@@ -1,6 +1,16 @@
-import { mainnetClient } from "@bgd-labs/js-utils";
+import { CHAIN_ID_CLIENT_MAP, mainnetClient } from "@bgd-labs/js-utils";
 import { GovernanceV3Ethereum } from "@bgd-labs/aave-address-book";
-import type { Client } from "viem";
-import { cacheGovernance } from "./src/updateCache";
+import type { Address, Client } from "viem";
+import { cacheGovernance, cachePayloadsController } from "./src/updateCache";
 
-await cacheGovernance(mainnetClient as Client, GovernanceV3Ethereum.GOVERNANCE)
+// 1. cache governance
+const cache = await cacheGovernance(mainnetClient as Client, GovernanceV3Ethereum.GOVERNANCE)
+const payloadsController = new Map<string, {chainId: number, controller: Address}>();
+Object.keys(cache.proposalsCache).map(key => {
+    const proposal = cache.proposalsCache[key];
+    proposal.payloads.map(payload => payloadsController.set(`${Number(payload.chain)}:${payload.payloadsController}`, {chainId: Number(payload.chain), controller: payload.payloadsController}))
+})
+// 2. cache each unique controller ever touched by governance
+for (const [,{chainId,controller}] of payloadsController) {
+    await cachePayloadsController(CHAIN_ID_CLIENT_MAP[chainId], controller);
+}
