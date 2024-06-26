683704f4ac70d50a657bd94aff129d5304728c93
sakulstra <lukasstrassel@googlemail.com>
2024-06-26 17:46:52 +0900

    fix: add missing logs

--- src/common/refreshCache.ts
+++ src/common/refreshCache.ts
@@ -14,27 +14,29 @@ import {
 import type {GovernanceCacheAdapterWithSync} from '..';
 
 export async function refreshCache(adapter: GovernanceCacheAdapterWithSync) {
+  console.time('cache');
   await adapter.syncProposalCache({
     chainId: 1,
     governance: GovernanceV3Ethereum.GOVERNANCE,
   });
+  console.timeEnd('cache');
 
-  [
-    GovernanceV3Ethereum,
-    GovernanceV3Arbitrum,
-    GovernanceV3Avalanche,
-    GovernanceV3Metis,
-    GovernanceV3Optimism,
-    GovernanceV3Gnosis,
-    GovernanceV3BNB,
-    GovernanceV3Polygon,
-    GovernanceV3PolygonZkEvm,
-    GovernanceV3Scroll,
-    GovernanceV3Base,
-  ].map(({PAYLOADS_CONTROLLER, CHAIN_ID}) => {
-    return adapter.syncPayloadsCache({
-      chainId: CHAIN_ID,
-      payloadsController: PAYLOADS_CONTROLLER,
-    });
-  });
+  // [
+  //   GovernanceV3Ethereum,
+  //   GovernanceV3Arbitrum,
+  //   GovernanceV3Avalanche,
+  //   GovernanceV3Metis,
+  //   GovernanceV3Optimism,
+  //   GovernanceV3Gnosis,
+  //   GovernanceV3BNB,
+  //   GovernanceV3Polygon,
+  //   GovernanceV3PolygonZkEvm,
+  //   GovernanceV3Scroll,
+  //   GovernanceV3Base,
+  // ].map(({PAYLOADS_CONTROLLER, CHAIN_ID}) => {
+  //   return adapter.syncPayloadsCache({
+  //     chainId: CHAIN_ID,
+  //     payloadsController: PAYLOADS_CONTROLLER,
+  //   });
+  // });
 }
