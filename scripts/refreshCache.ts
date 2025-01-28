f95737cd081f76c05712e6110f9b1e4db10731f4
Lukas <lukasstrassel@googlemail.com>
2025-01-28 11:35:06 +0100

    fix: update cache

--- scripts/refreshCache.ts
+++ scripts/refreshCache.ts
@@ -1,7 +1,6 @@
 import {customStorageProvider} from '../src/providers/customStorageProvider';
 import {refreshCache} from '../src/common/refreshCache';
 import {fileSystemStorageAdapter} from '../src/providers/storage/fileSystem';
-
 const provider = customStorageProvider(fileSystemStorageAdapter);
 
 refreshCache(provider);
